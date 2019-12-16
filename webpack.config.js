5c8f89cea784c6642fcf5e9d0058cd0634f6e296
Demidov Roman <demidovrv1@yandex.ru>
2019-12-16 22:52:05 +0300

    Task 7 of the project work 11 (style-loader)

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -19,7 +21,7 @@ module: {
             },
             {
                 test: /\.css$/, 
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] 
+                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'] 
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
@@ -48,6 +50,14 @@ module: {
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css',
         }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
+       }),
         new HtmlWebpackPlugin({
             inject: false, 
             template: './src/index.html', 
