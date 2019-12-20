350d2aba6afa03b7ea8ee9680946a57a5719448f
Demidov Roman <demidovrv1@yandex.ru>
2019-12-20 20:36:11 +0300

    Edited the file user-info__photo attached style guide

--- src/blocks/popup/popup.js
+++ src/blocks/popup/popup.js
@@ -1,15 +1,14 @@
-
 export class Popup {
   constructor(popupSelector) {
     this.popup = document.querySelector(popupSelector);
     this.popup.querySelector('.popup__close').addEventListener('click', this.closePopup);
   }
 
-  closePopup(event){   //метод закрытия попапа
+  closePopup(event) {   //метод закрытия попапа
     event.target.parentElement.parentElement.classList.remove('popup_is-opened');
   };
 
-  openForm(){   //метод открытия popup 
+  openForm() {   //метод открытия popup 
     this.popup.classList.toggle('popup_is-opened');
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 
 const path = require('path');
 const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -12,45 +12,45 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: { loader: "babel-loader" },
+            exclude: /node_modules/
         },
-module: {
-    rules: [{ 
-        test: /\.js$/, 
-        use: { loader: "babel-loader" }, 
-        exclude: /node_modules/ 
-            },
-            {
-                test: /\.css$/, 
-                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader'] 
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
+        {
+            test: /\.css$/,
+            use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
                     loader: 'file-loader',
-                  },
-                ],
-              },
-              {
-                test: /\.(png|jpg|gif|ico|svg)$/,
-                use: [
-                    'file-loader?name=./images/[name].[ext]',
-                    {
-                            loader: 'image-webpack-loader',
-                            options: {}
-                    },
-                ]
-               },
-               {
-                test: /\.(eot|ttf|woff|woff2)$/,
-                loader: 'file-loader?name=./vendor/[name].[ext]'
-                }
+                },
+            ],
+        },
+        {
+            test: /\.(png|jpg|gif|ico|svg)$/,
+            use: [
+                'file-loader?name=./images/[name].[ext]',
+                {
+                    loader: 'image-webpack-loader',
+                    options: {}
+                },
+            ]
+        },
+        {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'file-loader?name=./vendor/[name].[ext]'
+        }
         ]
     },
-    
+
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-     },
+    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css',
@@ -59,18 +59,18 @@ module: {
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano'),
             cssProcessorPluginOptions: {
-                    preset: ['default'],
+                preset: ['default'],
             },
             canPrint: true
-       }),
+        }),
         new HtmlWebpackPlugin({
-            inject: false, 
-            template: './src/index.html', 
-            filename: 'index.html'  
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
         }),
         new WebpackMd5Hash(),
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-           })
+        })
     ]
 };
